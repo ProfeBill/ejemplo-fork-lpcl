dbbb54df764f2ea0b17c0c0ed389a910b4bfc9e3
Andeerson <159457050+andersonmonsal@users.noreply.github.com>
2024-08-16 07:43:44 -0500

    Actualizar controllertest.py

--- casos de prueba/test/controllertest.py
+++ casos de prueba/test/controllertest.py
@@ -2,162 +2,109 @@ import unittest
 from datetime import datetime
 import os
 import sys
+
 directorio_actual = os.path.dirname(os.path.abspath(__file__))
 ruta_src = os.path.join(directorio_actual, '..', 'src')
 sys.path.insert(0, ruta_src)
+
 from model.calculadora import CalculadoraLiquidacion
 
-class TestCalculadoraLiquidacion(unittest.TestCase):
-    def setUp(self):
-        self.calculadora = CalculadoraLiquidacion()
+class LiquidacionTests(unittest.TestCase):
 
-    def test_calculo_liquidacion(self):
+    def setUp(self):
+        self.calc = CalculadoraLiquidacion()
+
+    def test_liquidacion_correcta(self):
         salario = 1500000
         fecha_inicio = "01/01/2022"
         fecha_fin = "01/01/2023"
         fecha_inicio_dt = datetime.strptime(fecha_inicio, "%d/%m/%Y")
         fecha_fin_dt = datetime.strptime(fecha_fin, "%d/%m/%Y")
         dias_trabajados = (fecha_fin_dt - fecha_inicio_dt).days
-        tiempo_trabajado_anos = dias_trabajados / 365
-        indemnizacion, _, _, _, _, _, _ = self.calculadora.calcular_resultados_prueba(
+        años_trabajados = dias_trabajados / 365
+
+        indemnizacion, *_ = self.calc.calcular_resultados_prueba(
             salario_basico=salario,
             fecha_inicio_labores=fecha_inicio,
             fecha_ultimas_vacaciones=fecha_fin,
             dias_acumulados_vacaciones=0
-    )
-        meses_maximos = 12
-        dias_por_anio = 20
-        dias_maximos = meses_maximos * dias_por_anio
-        dias_indemnizacion = min(tiempo_trabajado_anos * dias_por_anio, dias_maximos)
-        liquidacion_esperada = round((salario * dias_indemnizacion) / 30, 2)
-        self.assertEqual(indemnizacion, liquidacion_esperada)
+        )
 
-    def test_calculo_indemnizacion(self):
+        dias_por_anio = 20
+        max_dias_indemnizacion = min(años_trabajados * dias_por_anio, 12 * dias_por_anio)
+        liquidacion_calculada = round((salario * max_dias_indemnizacion) / 30, 2)
+
+        self.assertEqual(indemnizacion, liquidacion_calculada)
+
+    def test_indemnizacion(self):
         salario = 2500000
         meses_trabajados = 6
-        tiempo_trabajado_anos = meses_trabajados / 12  
-        valor_indemnizacion = self.calculadora.calcular_indemnizacion(salario, tiempo_trabajado_anos)
-        valor_esperado = round(salario * tiempo_trabajado_anos * 20 / 30, 2)
-        self.assertEqual(valor_indemnizacion, valor_esperado)
+        años_trabajados = meses_trabajados / 12
+        resultado_indemnizacion = self.calc.calcular_indemnizacion(salario, años_trabajados)
+        esperado = round(salario * años_trabajados * 20 / 30, 2)
 
-    def test_calculo_vacaciones(self):
+        self.assertEqual(resultado_indemnizacion, esperado)
+
+    def test_vacaciones(self):
         salario = 1500000
         dias_trabajados = 10
-        result = self.calculadora.calcular_vacaciones(salario, dias_trabajados)
-        self.assertAlmostEqual(result, 20833.33, places=2)
+        valor_vacaciones = self.calc.calcular_vacaciones(salario, dias_trabajados)
 
-    def test_calculo_cesantias(self):
-        salario_mensual = 3000000
-        dias_trabajados = 15
-        result = self.calculadora.calcular_cesantias(salario_mensual, dias_trabajados)
-        self.assertEqual(result, 125000)
+        self.assertAlmostEqual(valor_vacaciones, 20833.33, places=2)
 
-    def calcular_prima(self, salario_mensual, dias_trabajados):
-        prima = salario_mensual * (dias_trabajados / 180)  
-        return round(prima, 2)
+    def test_cesantias(self):
+        salario = 3000000
+        dias = 15
+        cesantias = self.calc.calcular_cesantias(salario, dias)
 
-    def test_calculo_retencion(self):
-        ingreso_laboral = 5000000
-        result = self.calculadora.calcular_retencion(ingreso_laboral)
-        self.assertEqual(result, 242349.75)
+        self.assertEqual(cesantias, 125000)
 
-    def test_formato_fecha_invalido_calculo_liquidacion(self):
+    def test_retencion(self):
+        ingreso = 5000000
+        retencion_calculada = self.calc.calcular_retencion(ingreso)
+
+        self.assertEqual(retencion_calculada, 242349.75)
+
+    def test_fechas_invalidas(self):
         salario = 2000000
-        fecha_inicio = "01-01-2022"
+        fecha_incorrecta = "01-01-2022"
         fecha_fin = "15-01-2022"
+
         with self.assertRaises(ValueError):
-            self.calculadora.calcular_resultados_prueba(salario, fecha_inicio, fecha_fin, 0)
+            self.calc.calcular_resultados_prueba(salario, fecha_incorrecta, fecha_fin, 0)
 
-    def test_calculo_intereses_cesantias_valor_negativo(self):
-        calc = CalculadoraLiquidacion()
-        cesantias = -10000
-        vacaciones = 20000
+    def test_indemnizacion_salario_negativo(self):
+        salario = -2000000
+        meses = 6
+        años = meses / 12
+
         with self.assertRaises(ValueError):
-            calc.calcular_intereses_cesantias(cesantias, vacaciones)
+            self.calc.calcular_indemnizacion(salario, años)
 
-    def test_calculo_liquidacion_fecha_invalida(self):
-        calc = CalculadoraLiquidacion()
-        salario_basico = 500000
-        fecha_inicio_labores = "01/01/2023"
-        fecha_ultimas_vacaciones = "30/02/2023" 
-        dias_acumulados_vacaciones = 10
-        with self.assertRaises(ValueError):
-            calc.calcular_resultados_prueba(salario_basico, fecha_inicio_labores, fecha_ultimas_vacaciones, dias_acumulados_vacaciones)
-
-    class TestCalculadoraLiquidacion(unittest.TestCase):
-        def test_motivo_invalido_calculo_indemnizacion(self):
-            salario = 2000000
-            meses_trabajados = 6
-            tiempo_trabajado_anos = meses_trabajados / 12
-            self.calculadora.calcular_indemnizacion(salario, tiempo_trabajado_anos)
-
-    def test_dias_trabajados_negativos_calculo_vacaciones(self):
-        salario_mensual = 2000000
-        dias_trabajados = -5
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_vacaciones(salario_mensual, dias_trabajados)
-
-    def test_dias_trabajados_negativos_calculo_cesantias(self):
-        salario_mensual = 2000000
-        dias_trabajados = -10
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_cesantias(salario_mensual, dias_trabajados)
-
-    def test_formato_ingreso_laboral_invalido_calculo_retencion(self):
-        ingreso_laboral = "5000000"
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_retencion(ingreso_laboral)
-
-    def test_total_pagar_negativo_imprimir_resultados(self):
-        pass
-
-    def test_formato_fecha_inicio_invalido(self):
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_resultados_prueba(2000000, "01-01-2022", "01/01/2023", 0)
-
-    def test_formato_fecha_ultimas_vacaciones_invalido(self):
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_resultados_prueba(2000000, "01/01/2022", "2023/01/01", 0)
-
-    def test_calculo_indemnizacion_maximo_dias(self):
+    def test_vacaciones_dias_negativos(self):
         salario = 2000000
-        meses_trabajados = 20  
-        tiempo_trabajado_anos = meses_trabajados / 12
-        indemnizacion = self.calculadora.calcular_indemnizacion(salario, tiempo_trabajado_anos)
-        meses_maximos = 12
-        dias_por_anio = 20
-        dias_maximos = meses_maximos * dias_por_anio
-        dias_indemnizacion = min(tiempo_trabajado_anos * dias_por_anio, dias_maximos)
-        indemnizacion_esperada = round((salario * dias_indemnizacion) / 30, 2)
-        self.assertEqual(indemnizacion, indemnizacion_esperada)
+        dias = -5
 
-    def test_salario_basico_negativo(self):
         with self.assertRaises(ValueError):
-            self.calculadora.calcular_resultados_prueba(-2000000, "01/01/2022", "01/01/2023", 0)
+            self.calc.calcular_vacaciones(salario, dias)
 
-    def test_dias_acumulados_vacaciones_negativos(self):
+    def test_retencion_ingreso_invalido(self):
+        ingreso = "cinco millones"
+
         with self.assertRaises(ValueError):
-            self.calculadora.calcular_vacaciones(2000000, -10)
+            self.calc.calcular_retencion(ingreso)
 
-    def test_tipo_ingreso_laboral_invalido(self):
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_retencion("five million")
-
-    def test_calculo_prima_dias_negativos(self):
-        salario_mensual = 2000000
-        dias_trabajados = -15
-        with self.assertRaises(ValueError):
-            self.calculadora.calcular_prima(salario_mensual, dias_trabajados)
-
-    def test_calculo_retencion_maximo_rango(self):
-        ingreso_laboral = 5000000000  
+    def test_retencion_maxima(self):
+        ingreso_alto = 5000000000  
         valor_uvt = 39205 
-        self.calculadora.valor_uvt = valor_uvt
-        retencion = self.calculadora.calcular_retencion(ingreso_laboral)
-        ingreso_uvt = ingreso_laboral / valor_uvt
+        self.calc.valor_uvt = valor_uvt
+
+        retencion = self.calc.calcular_retencion(ingreso_alto)
+        ingreso_uvt = ingreso_alto / valor_uvt
         base_uvt = ingreso_uvt - 2300
-        retencion_esperada = round(base_uvt * 0.39 * valor_uvt + 770 * valor_uvt, 2)
-        self.assertEqual(retencion, retencion_esperada)
+        esperado = round(base_uvt * 0.39 * valor_uvt + 770 * valor_uvt, 2)
+
+        self.assertEqual(retencion, esperado)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
